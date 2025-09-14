e23329b68139beb01b533f313f463af8bf8e8962
Tanish-07 <tanish14082004@gmail.com>
2025-09-14 06:04:46 +0530

    Added Configuration file reduce the work of hardcoding usernames, passwords, and URLs

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,31 +1,44 @@
 # tests/test_login.py
+
+# Notice we only import what's needed for the test logic
 from page_objects.login_page import LoginPage
 from page_objects.inventory_page import InventoryPage
+from utils.config_reader import read_config
 
 
-# The file now starts directly with the tests.
-# Pytest will automatically find the 'driver' fixture in conftest.py
+# The driver fixture is now automatically used from conftest.py
 
-# Test Case 1: Successful Login
 def test_successful_login(driver):
+    """
+    Tests a successful login using standard user credentials from the config file.
+    """
     login_page = LoginPage(driver)
     inventory_page = InventoryPage(driver)
 
+    # Get username and password from the config file
+    username = read_config("Credentials", "standard_user")
+    password = read_config("Credentials", "password")
+
     # Perform login
-    login_page.enter_username("standard_user")
-    login_page.enter_password("secret_sauce")
+    login_page.enter_username(username)
+    login_page.enter_password(password)
     login_page.click_login()
 
     # Assert that we are on the inventory page
-    assert inventory_page.is_inventory_page_displayed(), "Login failed and inventory page was not displayed."
+    assert inventory_page.is_inventory_page_displayed(), "Login failed: Inventory page not displayed."
 
 
-# Test Case 2: Failed Login
-def test_failed_login(driver):
+def test_failed_login_with_wrong_password(driver):
+    """
+    Tests a failed login attempt using a wrong password.
+    """
     login_page = LoginPage(driver)
 
+    # Get username from config, but use a deliberate wrong password
+    username = read_config("Credentials", "standard_user")
+
     # Perform login with wrong credentials
-    login_page.enter_username("standard_user")
+    login_page.enter_username(username)
     login_page.enter_password("wrong_password")
     login_page.click_login()
 
---
+++ utils/config_reader.py
@@ -0,0 +1,7 @@
+# utils/config_reader.py
+import configparser
+
+def read_config(section, key):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config.get(section, key)
